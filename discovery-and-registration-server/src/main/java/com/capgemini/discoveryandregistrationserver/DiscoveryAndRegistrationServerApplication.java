9b7ccada4bc6ec270e85da18a603648a22fc83dd
adkeaesph <35371920+adkeaesph@users.noreply.github.com>
2020-02-04 11:06:49 +0530

    registration of hotels db mgmt service on discovery and registration service

--- discovery-and-registration-server/src/main/java/com/capgemini/discoveryandregistrationserver/DiscoveryAndRegistrationServerApplication.java
+++ discovery-and-registration-server/src/main/java/com/capgemini/discoveryandregistrationserver/DiscoveryAndRegistrationServerApplication.java
@@ -2,8 +2,10 @@ package com.capgemini.discoveryandregistrationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryAndRegistrationServerApplication {
 
 	public static void main(String[] args) {
--- hotels-db-mgmt-service/src/main/java/com/capgemini/hotelsdbmgmtservice/HotelsDbMgmtServiceApplication.java
+++ hotels-db-mgmt-service/src/main/java/com/capgemini/hotelsdbmgmtservice/HotelsDbMgmtServiceApplication.java
@@ -2,9 +2,10 @@ package com.capgemini.hotelsdbmgmtservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class HotelsDbMgmtServiceApplication {
 
 	public static void main(String[] args) {
