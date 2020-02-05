4fe1fabfddc9bdb56a21ab90004d91189184d540
adkeaesph <35371920+adkeaesph@users.noreply.github.com>
2020-02-06 01:03:25 +0530

    minor changes

--- hotels-catalogue-mgmt-service/src/main/java/com/capgemini/hotelscataloguemgmtservice/controller/HotelCatalogueSuperController.java
+++ hotels-catalogue-mgmt-service/src/main/java/com/capgemini/hotelscataloguemgmtservice/controller/HotelCatalogueSuperController.java
@@ -1,7 +1,6 @@
 package com.capgemini.hotelscataloguemgmtservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
