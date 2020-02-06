7dbc09a3b5e4ab57af2c21fc39ba211034933a4d
adkeaesph <35371920+adkeaesph@users.noreply.github.com>
2020-02-06 17:18:00 +0530

    removed unnecessary services

--- hotel-db-mgmt-service/src/main/java/com/capgemini/hoteldbmgmtservice/controller/HotelBookingController.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.hoteldbmgmtservice.controller;
-
-public class HotelBookingController {
-
-	public HotelBookingController() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- hotel-db-mgmt-service/src/main/java/com/capgemini/hoteldbmgmtservice/controller/HotelExplorationController.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.hoteldbmgmtservice.controller;
-
-public class HotelExplorationController {
-
-	public HotelExplorationController() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
