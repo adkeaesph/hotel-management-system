4a8cb82c34d77bf7411fa76b2fa0b46e0ac42ffb
AMAN RAJ <amanraj98@users.noreply.github.com>
2020-02-06 13:43:18 +0530

    changes
    cross origin added

--- hotels-catalogue-mgmt-service/src/main/java/com/capgemini/hotelscataloguemgmtservice/controller/HotelCatalogueSuperController.java
+++ hotels-catalogue-mgmt-service/src/main/java/com/capgemini/hotelscataloguemgmtservice/controller/HotelCatalogueSuperController.java
@@ -1,6 +1,7 @@
 package com.capgemini.hotelscataloguemgmtservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.client.RestTemplate;
 import com.capgemini.hotelscataloguemgmtservice.dto.Hotel;
 import com.capgemini.hotelscataloguemgmtservice.dto.HotelList;
 
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(path = "/hotel-catalogue")
 public class HotelCatalogueSuperController {
