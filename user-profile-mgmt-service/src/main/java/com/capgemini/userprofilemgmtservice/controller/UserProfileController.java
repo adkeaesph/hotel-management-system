9e54d0dc90ab1ca3955524fc9ca6c6b3732c8208
adkeaesph <35371920+adkeaesph@users.noreply.github.com>
2020-02-11 13:13:43 +0530

    updated user profile controller

--- user-profile-mgmt-service/src/main/java/com/capgemini/userprofilemgmtservice/controller/UserProfileController.java
+++ user-profile-mgmt-service/src/main/java/com/capgemini/userprofilemgmtservice/controller/UserProfileController.java
@@ -4,25 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.capgemini.userprofilemgmtservice.customexceptions.AddBookingLogException;
-import com.capgemini.userprofilemgmtservice.customexceptions.UserRegisterException;
 import com.capgemini.userprofilemgmtservice.dto.Reservation;
-import com.capgemini.userprofilemgmtservice.dto.ReservationList;
 import com.capgemini.userprofilemgmtservice.dto.StayIdListWithEmailId;
-import com.capgemini.userprofilemgmtservice.dto.User;
 import com.capgemini.userprofilemgmtservice.services.UserProfileService;
 import com.capgemini.userprofilemgmtservice.services.UserService;
 
 @RequestMapping(path = "/user-profile")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class UserProfileController {
 	@Autowired
@@ -30,7 +28,7 @@ public class UserProfileController {
 
 	@Autowired
 	UserService userService;
-	
+
 	@Autowired
 	RestTemplate restTemplate;
 
@@ -46,7 +44,7 @@ public class UserProfileController {
 	}
 
 	@GetMapping("/view-my-bookings/{emailID}")
-	public ReservationList viewMyBookings(@PathVariable("emailID") String emailID) {
+	public List<Reservation> viewMyBookings(@PathVariable("emailID") String emailID) {
 		List<Integer> stayIDs = userProfileService.getStayIDs(emailID);
 		Reservation reservation = null;
 		List<Reservation> reservations = new ArrayList<>();
@@ -59,8 +57,6 @@ public class UserProfileController {
 		}
 		if (reservations.size() == 0)
 			return null;
-		ReservationList reservationList = new ReservationList();
-		reservationList.setReservations(reservations);
-		return reservationList;
+		return reservations;
 	}
 }
